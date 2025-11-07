c7af250ad85bbbe65cf282991a5d54b75d17858d
BriandaCampoy <62039999+BriandaCampoy@users.noreply.github.com>
2025-11-06 22:34:36 -0700

    Allow cors

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 require('./auth/index')
 
